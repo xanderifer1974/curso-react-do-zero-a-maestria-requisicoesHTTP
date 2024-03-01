166511663b23eac9903c73789ca437ae1a74b902
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-01 17:12:04 -0300

    Resgatando dados da API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fechData() {
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setProducts(data);
+    }
+    fechData();
+  }, []);
+
+  console.log(products);
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
