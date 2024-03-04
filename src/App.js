f79f7c07a507be560b4b0a257c49ec4b340f8d32
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2024-03-04 15:39:45 -0300

    Adicionando dados com React

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [categoryId, setCategoryId] = useState("");
 
+  // 1 - Resgatando dados
   useEffect(() => {
     async function fechData() {
       const resp = await fetch(url);
@@ -16,7 +20,28 @@ function App() {
     fechData();
   }, []);
 
-  console.log(products);
+  //2 - Adição de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product ={
+      name,
+      price,
+      categoryId
+    };
+
+    const res = await fetch(url,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify(product),
+    });
+
+    
+
+
+  }; 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
@@ -24,11 +49,44 @@ function App() {
         {products.map((product) => {
           return (
             <li key={product.id}>
-              {product.name} - R$: {product.price}
+              {product.name} - R$: {product.price} - IdCategoria: {product.categoryId}
             </li>
           );
         })}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <label>
+            IdCategoria:
+            <input
+              type="number"
+              value={categoryId}
+              name="categoryId"
+              onChange={(e) => setCategoryId(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar Produto" />
+        </form>
+      </div>
     </div>
   );
 }
